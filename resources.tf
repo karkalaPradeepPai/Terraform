07ef63fa766c3c5acf4c7aaebf71cf2c49b56774
Karkala <pradeeppai.karkala@nuance.com>
2019-09-10 14:03:42 -0400

    Committing 2nd resources

--- resources.tf
+++ resources.tf
@@ -6,3 +6,29 @@ resource "aws_vpc" "environment-example" {
     Name = "terraform-aws-vpc-example"
   }
 }
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example.id}"
+  availability_zone = "us-east-2a"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example.id}"
+  availability_zone = "us-east-2b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.environment-example.id}"
+
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-example.cidr_block}"
+    ]
+
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+}
